9e9953bfada87f0a73e149542186675a6f35d4d5
HBazai <bazaee.hassan@gmail.com>
2023-06-30 12:22:32 +0330

    Upper Panel colors handeling

---
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -0,0 +1,63 @@
+package com.example.littlelemon
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun DishDetails(id: Int) {
+    val dish = requireNotNull(DishRepository.getDish(id))
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        Counter()
+    }
+}
+
+@Composable
+fun Counter() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        var counter by remember {
+            mutableStateOf(1)
+        }
+        TextButton(
+            onClick = {
+                counter--
+            }
+        ) {
+            Text(
+                text = "-",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+        Text(
+            text = counter.toString(),
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+        TextButton(
+            onClick = {
+                counter++
+            }
+        ) {
+            Text(
+                text = "+",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun DishDetailsPreview() {
+    DishDetails(id = 1)
+}
---
+++ app/src/main/java/com/example/littlelemon/UpperPanel.kt
@@ -0,0 +1,78 @@
+package com.example.littlelemon
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.littlelemon.ui.theme.LittleLemonColor.cloud
+import com.example.littlelemon.ui.theme.LittleLemonColor.green
+import com.example.littlelemon.ui.theme.LittleLemonColor.yellow
+
+@Composable
+fun UpperPanel() {
+    Column(
+        modifier = Modifier
+            .background(green)
+            .padding(start = 12.dp, end = 12.dp, top = 16.dp, bottom = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.title),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = yellow
+        )
+        Text(
+            text = stringResource(id = R.string.location),
+            fontSize = 24.sp,
+            color = cloud
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(top = 20.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.description),
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .padding(bottom = 28.dp, end = 20.dp)
+                    .fillMaxWidth(0.6f)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.upperpanelimage),
+                contentDescription = "Upper Panel Image",
+                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+            )
+        }
+        Button(
+            onClick = {
+                      // TODO set navigation
+            },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = yellow)
+        ) {
+            Text(
+                text = stringResource(id = R.string.order_button_text)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UpperPanelPreview() {
+    UpperPanel()
+}
